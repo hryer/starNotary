5c8efd89df491b303c8e4d843d1f851afa607b83
Harry Ermawan <harryermawan@gmail.com>
2021-04-26 01:08:06 +0700

    fix: fix breaking changes solidity

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -10,6 +10,10 @@ contract StarNotary {
         starName = "Awesome Udacity Star";
     }
 
+    function changeName(string memory _name) public {
+        starName = _name;
+    }
+
     function claimStar() public {
         starOwner = msg.sender;
         emit starClaimed(msg.sender);
