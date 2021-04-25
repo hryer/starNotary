8c3deaa59fcff25bd49d1d7a6e17795401310120
Harry Ermawan <harryermawan@gmail.com>
2021-04-26 01:02:13 +0700

    chore: add testing

---
+++ contracts/StarNotary.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract StarNotary {
+    string public starName;
+    address public starOwner;
+
+    event starClaimed(address owner);
+
+    constructor() public {
+        starName = "Awesome Udacity Star";
+    }
+
+    function claimStar() public {
+        starOwner = msg.sender;
+        emit starClaimed(msg.sender);
+    }
+}
